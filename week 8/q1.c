567ad2c00b824c5ee3cad43591f8e9130bd83f55
EnesTunc <115104394+EnesTunc@users.noreply.github.com>
2023-04-25 10:39:52 +0300

    Add files via upload

---
+++ week 8/q1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() 
+{
+	struct x
+	{
+    int numara;
+    char isim[10];
+    char soyisim[10];
+    int puan;
+    }
+
+    ogrenciler[10];
+    int i;
+
+    for (i=0;i<3;i++) 
+	{
+        printf("%d. ogrencinin numarasini girin==>",i+1);
+        scanf("%d",&ogrenciler[i].numara);
+        
+        printf("%d.ogrencinin adini girin==>", i+1);
+        scanf("%s",&ogrenciler[i].isim);
+        
+        printf("%d.ogrencinin soyismini girin==>",i+1);
+        scanf("%s",&ogrenciler[i].soyisim);
+        
+        printf("%d.ogrencinin puanini girin==>",i+1);
+        scanf("%d",&ogrenciler[i].puan);
+        
+        printf("\n\n");
+    }
+    printf("Ogrencilerin Bilgileri:\n");
+    for (i=0;i<3;i++) 
+	{
+        printf("Numarasi==>%d\n",ogrenciler[i].numara);
+        printf("ismi==>%s\n",ogrenciler[i].isim);
+        printf("Soyismi==>%s\n",ogrenciler[i].soyisim);
+        printf("Sinav Puani==>%d\n",ogrenciler[i].puan);
+        printf("***\n");
+    }
+     return 0;
+}
+
